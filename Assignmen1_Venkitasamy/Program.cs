dfe981ad0f926a39670d54b12a2495142c6cc778
yagnad <yagnadheepik@usf.edu>
2019-01-24 15:37:21 -0500

    mid commit..

--- Assignmen1_Venkitasamy/Program.cs
+++ Assignmen1_Venkitasamy/Program.cs
@@ -6,21 +6,142 @@ namespace Assignmen1_Venkitasamy
     {
         public static void Main()
         {
+            //Gather user input to calculate results
+            Console.WriteLine("Hi! Choose from the following operations: " +
+                "\n 1: Print Prime Numbers" +
+                "\n 2: Get series result" + 
+                "\n 3: Decimal to Binary conversion" +
+                " \n 4: Binary to Decimal Conversion " +
+                "\n 5: Print Star Triange" + 
+                "\n 6: Compute frequency in an array " +
+                "\n 7: Exit");
+
+            int userSelection = Convert.ToInt32(Console.ReadLine());
+            //Convert the user input to integer value
+
+            //Using try block to validate the user input 
+            try
+            {
+                
+                    switch (userSelection)
+                    {
+                        //Print Prime nos case
+                        case 1:
+                            //Displaying the user choice
+                            Console.WriteLine("You have chosen to print prime numbers between 2 integers");
+
+                            Console.WriteLine("Please input the value of X: ");
+                            string X_input = Console.ReadLine();//Gathering user input
+                            int X = int.Parse(X_input);//Converting user input
+
+                            Console.WriteLine("Please input the value of Y: ");
+                            string Y_input = Console.ReadLine();//Gathering user input
+                            int Y = int.Parse(Y_input);//Converting user input
+
+                            printPrimeNos(X,Y); //Calls the void method to execute the calculation
+                            
+                            break;
+
+                        //Get the series value
+                        case 2:
+                            //Displaying the user choice
+                            Console.WriteLine("You have chosen to get series result for N");
+
+                            Console.WriteLine("Input the number N: ");
+                            int n = Convert.ToInt32(Console.ReadLine());
+
+                            printFactorial(n);//Calling the return method to calculate the series value of N
+
+                            break;
+                        
+                        //Decimal to binary conversion case
+                        case 3:
+
+                            //Displaying the user choice
+                            Console.WriteLine("You have chosen to convert the decimal to binary for N");
+
+                            Console.WriteLine("Input the number N: ");//Gathering user input
+                            int a = Convert.ToInt32(Console.ReadLine());//Converting user input
+
+                            decimalToBinary(a);//Calls the return method to display the binary conversion
+
+                            break;
+
+                        //Binary too decimal conversion
+                        case 4:
+                            //Displaying the user choice
+                            Console.WriteLine("You have chosen to convert the binary to Decimal for N");
+
+                            Console.Write("Enter a Binary Number(1s and 0s) : ");//Gathering user input
+                            int b = int.Parse(Console.ReadLine()); //Converting user input
+
+                            binaryToDecimal(b);//Calls the return method to display the decimal conversion
+
+                            break;
+
+                        //Print traingle
+                        case 5:
+                            //Displaying the user choice
+                            Console.WriteLine("You have chosen to print triangle using * for N");
+                            Console.WriteLine("Please input the number of rows desired: ");
+                            
+                            int k = int.Parse(Console.ReadLine());
+                            printTriange(k);//Calls the method to print traingle using *
+                            
+                            break;
+
+                        //Compute frquency in the array
+                        case 6:
+                            //Displaying the user choice
+                            Console.WriteLine("You have chosen to compute the frequency of elements in the array");
+                            Console.WriteLine("Enter the Maximum Range for the Array");
+                            
+                            break;
+
+                        //Exit program
+                        case 7:
+
+                            Console.WriteLine("Now exiting the console..!");//Display the exit message
+                            break;
+
+                        //Default message incase user does not select valid option
+                        default:
+
+                            Console.WriteLine("Please enter valid integers only next time..");
+                            break;
+                        
+
+                    }//End of switch
+               
+                Console.WriteLine("\nPress any key to exit the program ...");
+                Console.ReadKey(true);
+
+            }//End of try block
+
+            //Using catch block to display message in case of bad user input
+            catch
+            {
+                Console.WriteLine("Please input valid integers only..");
+                Console.WriteLine("\nPress any key to exit the program ...");
+                Console.ReadKey(true);
+            }//End of catch block
+
+        }//End of Main
+
+        private static void printPrimeNos(int X, int Y)
+
+        {
             //This method prints all the prime numbers between X and Y
             Console.WriteLine("Find all prime numbers between 2 integers X and Y: ");
 
-            int X, Y, i, A, N;
-            
+            //Declare the variables to use in the calculation
+            int i, A, N;
+
+            //Using the try block to validate user input
             try
             {
 
-                Console.WriteLine("Pls input X: ");//Input starting number
-                X = Convert.ToInt32(Console.ReadLine());//Converts the data to int
-
-                Console.WriteLine("Pls input Y: ");//Input ending number
-                Y = Convert.ToInt32(Console.ReadLine());//Converts the data to int
-
-                Console.Write("The prime numbers between {0} and {1} are : \n", X, Y);//Display the message
+                Console.Write("The prime numbers between {0} and {1} are : \n", X, Y);//Display the message based on user input
 
                 //For loop to calculate the prime numbers in the range
                 for (N = X; N <= Y; N++)
@@ -37,20 +158,136 @@ namespace Assignmen1_Venkitasamy
                     }
                     if (A == 0 && N != 1)
 
-                        Console.Write("{0} ",N);
-                        
-                }
-                Console.WriteLine("\nPress any key to exit the program ...");
-                Console.ReadKey(true);
-            }
+                        Console.Write("{0} ", N);
+
+                }//End of for loop
+
+            }//End of try block
+
+            //Using catch block to show error message for bad input
             catch
             {
-                Console.WriteLine("Pls enter a valid integer..");
+                Console.WriteLine("Exception occured while computing printPrimeNumbers()");
                 Console.WriteLine("\nPress any key to exit the program ...");
                 Console.ReadKey(true);
+            } //End of catch block
+        } //End of the method printPrimeNos
+
+        //Method to compute factorial values
+        private static double printFactorial(int n)
+        {
+            int i, f;
+            f = 1;
+            
+            for (i = 1; i <= n; i++)
+
+                f = f * i;
+
+            Console.WriteLine("The factorial of {0} is : {1}\n", n, f);
+            
+            return n;
+            
+        }//End of the factorial method
+
+        private static long decimalToBinary(long a)
+        {
+            long quot;
+            string rem = "";
+            while (a >= 1)
+            {
+                quot = a / 2;
+                rem += (a % 2).ToString();
+                a = quot;
             }
+            string bin = "";
+            for (int i = rem.Length - 1; i >= 0; i--)
+            {
+                bin = bin + rem[i];
+            }
+            Console.WriteLine("The Binary format for given number is {0}", bin);
+            
+            return a;
+            
+        }//End of DecimaltoBinary Method
+
+        private static long binaryToDecimal(long b)
+        {
+            long binary_val, decimal_val = 0, base_val = 1, rem;
+            
+            binary_val = b;
+            while (b > 0)
+            {
+                rem = b % 10;
+                decimal_val = decimal_val + rem * base_val;
+                b = b / 10;
+                base_val = base_val * 2;
+            }
+            Console.Write("The Binary Number is : " + binary_val);
+            Console.Write("\nIts Decimal Equivalent is : " + decimal_val);
+
+            return b;
+        }//End of the method binaryToDecimal
+
+        private static void printTriange(int k)
+
+        {
+            int number, i,  count = 1;
+            number = k;
+            count = number - 1;
+            for (k = 1; k <= number; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * k - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
             }
 
-        }
-    }
+        }//End of print triangle method
+
+
+        private static void computeFrequency(int []a)
+        {
+            int i, j, N, count;
+
+            Console.WriteLine("Enter the Maximum Range for the Array");
+            N = Convert.ToInt32(Console.ReadLine());
+            string[] m = new string[N];
+            int[] freq = new int[N];
+            for (i = 0; i < N; i++)
+            {
+                m[i] = Console.ReadLine();
+                freq[i] = -1;
+            }
+            for (i = 0; i < N; i++)
+            {
+                count = 1;
+                for (j = i + 1; j < N; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        count++;
+                        freq[j] = 0;
+                    }
+                    
+                }
+                if (freq[i] != 0)
+                {
+                    freq[i] = count;
+                }
+
+            }
+            for (i = 0; i < N; i++)
+            {
+                if (freq[i] != 1)
+                {
+                    Console.Write("{0}{1}", a[i], freq[i]);
+                }
+            }
+
+        } //End of computeFrequency method
+
+    }//End of Class
+} //End of namespace
 
